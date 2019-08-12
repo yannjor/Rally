7df36631cede3d8e9b398c9b604830f8b3eb5cbc
Yann <yannjorelle@gmail.com>
2019-08-12 12:49:53 +0300

    implemented write_results

--- Rally.c
+++ Rally.c
@@ -8,21 +8,21 @@
 /*Adds a driver named "lastname" and who is representing "team" to buffer of length "size".
   Returns 1 if the operation was successful, 0 otherwise.*/
 
-int add_driver(struct driver *buffer, char *lastname, char *team) {
+int add_driver(driver *buffer, char *lastname, char *team) {
     int i;
     for (i = 0; buffer[i].lastname[0] != '\0'; i++) //Find out the size of buffer
     
-    buffer = realloc(buffer, (i + 2) * sizeof(struct driver));
+    buffer = realloc(buffer, (i + 2) * sizeof(driver));
     if (!buffer) return 0;  //if memory allocation failed
     strcpy(buffer[i].lastname, lastname);
     strcpy(buffer[i].team, team);
     buffer[i].time = 0;  //Initially the total time is 0
-    buffer[i+1].lastname[0] = 0;
+    buffer[i+1].lastname[0] = '\0';
     return 1;
 }
 
 
-void update_time(struct driver *buffer, char *lastname, int time) {
+void update_time(driver *buffer, char *lastname, int time) {
     int flag = 0;
     for (int i = 0; buffer[i].lastname[0] != '\0'; i++) {
         if (!strcmp(buffer[i].lastname, lastname)) {
@@ -36,20 +36,19 @@ void update_time(struct driver *buffer, char *lastname, int time) {
 //Helper function for print_results:
 
 int compare (const void* p1, const void* p2) {
-    struct driver a = *(struct driver*)p1;
-    struct driver b  = *(struct driver*)p2;
+    driver a = *(driver*)p1;
+    driver b  = *(driver*)p2;
     if (a.time < b.time) return -1;
     else if (a.time > b.time) return 1;
     else return 0;
 }
 
 
-
-void print_results(struct driver *buffer) {
+void print_results(driver *buffer) {
     //First sort the buffer
     int j;
     for (j = 0; buffer[j].lastname[0] != '\0'; j++);    
-    qsort(buffer, j, sizeof(struct driver), compare);
+    qsort(buffer, j, sizeof(driver), compare);
 
     //Then print
     printf("Current results of the race:\n\n");
@@ -60,24 +59,61 @@ void print_results(struct driver *buffer) {
 }
 
 
-int write_results(const char *filename) {
+int write_results(driver *buffer, const char *filename) {
+    FILE *fp = fopen(filename, "w");
+    if (!fp) return 0;
 
+    for (int i = 0; buffer[i].lastname[0] != '\0'; i++) {
+        fprintf(fp, "%s - %s - %dh %dmin %ds\n", buffer[i].lastname, buffer[i].team, buffer[i].time / 3600,
+        (buffer[i].time % 3600) / 60, (buffer[i].time % 3600) % 60);
+    }
+    fclose(fp);
+    return 1;
 }
 
+
+//Doesn't currently work...
+
+int load_results(driver *buffer, const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return 0;
+
+    buffer = realloc(buffer, sizeof(driver));
+    char row[100];
+    int i = 0;
+
+    while(fgets(row, 100, fp)) {
+        buffer = realloc(buffer, sizeof(driver) * (i + 2));
+        int h, min, sec;
+        sscanf(row, "%s - %s - %dh %dmin %ds\n", buffer[i].lastname, buffer[i].lastname, &h, &min, &sec);
+        buffer[i].time = (h * 3600) + (min * 60) + sec;
+        i++;
+    }
+    buffer = realloc(buffer, sizeof(driver) * (i + 2));
+    buffer[i+1].lastname[0] = '\0';
+    return 1;
+}
+
+void exit_program(driver *buffer) {
+    printf("Exiting program...\n");
+    free(buffer);
+    exit(0);
+}
 
 
 int main() {
     
-    struct driver *buffer = malloc(sizeof(struct driver));
-    buffer->lastname[0] = 0;
+    driver *buffer = malloc(sizeof(driver));
+    buffer->lastname[0] = '\0';
 
-    
     add_driver(buffer, "Kankkunen", "Renault");
     update_time(buffer, "Kankkunen", 4325);
     add_driver(buffer, "Räikkönen", "Peugeot");
     update_time(buffer, "Räikkönen", 3333);
     add_driver(buffer, "Svakar Teknolog", "Lada");
-    update_time(buffer, "Svakar Teknolog", 3333333);
+    update_time(buffer, "Svakar Teknolog", 333333);
     print_results(buffer);
+    write_results(buffer, "results.txt");
+
     return 1;
 }
--- Rally.h
+++ Rally.h
@@ -1,10 +1,10 @@
 #ifndef YJ_RALLY_H
 #define	YJ_RALLY_H
 
-struct driver {
+typedef struct {
     char lastname[30];
     char team[30];
     int time;
-};
+} driver;
 
 #endif
